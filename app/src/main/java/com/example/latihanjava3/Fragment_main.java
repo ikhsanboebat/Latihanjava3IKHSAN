61bfc9d6f45b37adfb334f155ccdda6dd97a84fb
Ikhsan Maulana <9cikhsanmaulana@gmail.com>
2019-09-09 09:54:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanjava3/Fragment_main.java
@@ -0,0 +1,14 @@
+package com.example.latihanjava3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fragment_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_main);
+    }
+}
